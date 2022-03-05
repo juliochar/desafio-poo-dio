e1824eef74b97efd4a4548035cc8736e75bd4b48
julio <joufps@yahoo.com>
2022-03-05 20:11:33 -0300

    parte final com a classe principal funcionando.

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -12,22 +11,54 @@ public class ClassePrincipal {
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso1.setTitulo("Curso JavaScript");
-        curso1.setDescricao("Descrição curso JavaScript");
-        curso1.setCargaHoraria(4);
+        curso2.setTitulo("Curso JavaScript");
+        curso2.setDescricao("Descrição curso JavaScript");
+        curso2.setCargaHoraria(4);
+
+
+        //como conteudo nao tem setCargaHoraria, ele nao permite acessar o método
+        //teria q converter para Curso para acessar o metodo q só tem em Curso.
+
+        Conteudo conteudo = new Curso();
+        conteudo.setTitulo("titulo conteudo");
+        conteudo.setDescricao("Descricao Conteudo");
+
+
 
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("mentoria de Java");
         mentoria1.setDescricao("Descrição mentoria de Java");
         mentoria1.setData(LocalDate.now());
 
+        //criar o bootcamp, adicionar os cursos, mentorias e adicionar o dev no bootcamp
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("BootCamp de Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+        Dev devJonas = new Dev();
+        devJonas.setNome("Jonas");
+        devJonas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos inscritos: " + devJonas.getConteudosInscritos());
+        devJonas.progredir();
+        devJonas.progredir();
+        System.out.println("Conteúdos concluídos: " + devJonas.getConteudosConcluidos());
+        System.out.println("XP: " + devJonas.calcularXp());
 
+        System.out.println("------------------------------------------------");
+        System.out.println("------------------------------------------------");
 
-
+        Dev devPedro = new Dev();
+        devPedro.setNome("Pedro");
+        devPedro.inscreverBootcamp(bootcamp);
+        devPedro.progredir();
+        devPedro.progredir();
+        devPedro.progredir();
+        System.out.println("Conteúdos inscritos: " + devPedro.getConteudosInscritos());
+        System.out.println("Conteúdos concluidos: " + devPedro.getConteudosConcluidos());
+        System.out.println("XP: " + devPedro.calcularXp());
 
 
 
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,26 +2,9 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo {
+
     LocalDate data;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getData() {
         return data;
@@ -31,12 +14,19 @@ public class Mentoria {
         this.data = data;
     }
 
+
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO + 20d;
+
+    }
+
     @Override
     public String toString() {
         return "{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", data=" + data +
-                '}';
+                "Titulo" + getTitulo() +
+                "Descrição: " + getDescricao() +
+                "data=" + data +
+                "}";
     }
 }
